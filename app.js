6e24e7d18a0b246fc7fe78d0e3b44f3a40542cf6
Steveen <steveen.echeverri@gmail.com>
2020-01-08 22:36:14 -0500

    variables and class js

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-window.onload = () =>{
-    
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+let cart = [];
+
+class Products{
+    async getProducts(){
+        try {
+            let result = await fetch("products.json")
+            return result;
+        } catch (error) {
+            console.log(Errors);
+        }
+    }
 }
+
+class UI{
+
+}
+
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const ui = new UI();
+    const products = new Products();
+
+    products.getProducts().then(data => console.log(data));
+})
